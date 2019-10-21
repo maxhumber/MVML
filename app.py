b6b96351e40ce32882ef6f5d36acc5ff13094cc1
Max Humber <max.humber@gmail.com>
2019-10-21 16:33:49 -0400

    centred

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neighbors import NearestNeighbors
 from flask import Flask, request, render_template
 
-app = Flask(__name__, template_folder="basic")
+app = Flask(__name__, template_folder="templates")
 
 with open("model/model.pkl", "rb") as f:
     import dill
