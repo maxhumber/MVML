6eb1b8b34d2e71bd0fa7034fd85f289e69cc5163
Max Humber <max.humber@gmail.com>
2020-03-04 10:58:51 +0700

    exercise update

--- exercises/move_fast.py
+++ exercises/move_fast.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn.linear_model import LinearRegression, RANSACRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
-import mummify
+
+# <IMPORT MUMMIFY HERE>
 
 n_samples = 1000
 n_outliers = 50
@@ -25,11 +26,11 @@ y[:n_outliers] = -3 + 10 * np.random.normal(size=n_outliers)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42)
 
-model = LinearRegression()
+model = LinearRegression() # TRY A DIFFERENT MODEL HERE
 model.fit(X_train, y_train)
 model.score(X_test, y_test)
 
 with open('exercises/model.pkl', 'wb') as f:
     pickle.dump(model, f)
 
-mummify.log('') # put something in here
+# <LOG MUMMIFY HERE>
